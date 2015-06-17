8547bb643dbf7e590f110288860978126ea35879
Simon de Haan <simon@praekeltfoundation.org>
2015-06-17 12:12:10 +0200

    video fixes

--- video/js/video.js
+++ video/js/video.js
@@ -1,20 +1,23 @@
 var capture;
 var slider;
 
+video_width = 380;
+video_height = 240;
+
 function setup () {
-    frameRate(1);
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(video_width, video_height);
     capture = createCapture(VIDEO);
+    capture.hide();
     slider = createSlider(10, 100, 20);
     slider.position(20, 20);
 };
 
 function draw () {
     size = slider.value();
-    image(capture, 0, 0, windowWidth, windowHeight);
+    image(capture, 0, 0, video_width, video_height);
     loadPixels();
-    for(var x = 0; x < windowWidth; x += size) {
-        for(var y = 0; y < windowHeight; y += size) {
+    for(var x = 0; x < video_width; x += size) {
+        for(var y = 0; y < video_height; y += size) {
             c = get(x, y);
             for(var px = x; px < (x + size); px++) {
                 for(var py = y; py < (y + size); py++) {
